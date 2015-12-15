9653ea3b17d6c463542a25f8cf3cbede3f4de1a4
dtsarkov <dtdev001@tpg.com.au>
2015-12-16 07:17:43 +1100

    version 0.16

---
+++ CodeGenerator/src/com/github/dtsarkov/ea/tools/codegenerator/EAElement.java
@@ -0,0 +1,12 @@
+package com.github.dtsarkov.ea.tools.codegenerator;
+
+public class EAElement {
+	private Object element;
+	private Object elementParent;
+	private Object elementPackage;
+	
+	public EAElement(Object element) {
+		this.element = element;
+	}
+
+}
