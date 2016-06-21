6309a9f6d45f3c6a65921ab17093fec5f9068036
Dmitriy Tsarkov <dtdev001@tpg.com.au>
2016-06-21 22:21:09 +1000

    version 0.43

---
+++ CodeGenerator/src/com/github/dtsarkov/ea/tools/codegenerator/GlobalFilter_test.java
@@ -0,0 +1,34 @@
+package com.github.dtsarkov.ea.tools.codegenerator;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import com.github.dtsarkov.ea.tools.codegenerator.parser.EACodeTemplateLexer;
+import com.github.dtsarkov.ea.tools.codegenerator.parser.EACodeTemplateParser;
+
+public class GlobalFilter_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public GlobalFilter_test() { /*
+		InputStream 			is 		= new FileInputStream(fileName);
+		ANTLRInputStream  		input 	= new ANTLRInputStream(is);
+		
+		lexer 	= new EACodeTemplateLexer(input);
+		tokens	= new CommonTokenStream(lexer);
+		parser 	= new EACodeTemplateParser(tokens);
+		*/
+	}
+
+	public void addFilterCondition(String condition) {
+		
+	}
+
+}
